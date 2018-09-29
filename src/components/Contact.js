6039c9cf72fb65914897ada5ccede078b3cb1e5c
unknown <frantisek.dvorak011@gmail.com>
2018-09-29 10:22:11 +0200

    contact page styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Contact extends Component {
     state = {  }
     render() { 
         return ( 
-            <div className='main-page'>
+            <div className='main-page contact-body'>
                 <h1>Contact page</h1>
+                <Grid className='contact-grid'>
+                    <Cell col={6}>half page</Cell>
+                    <Cell col={6}>half page</Cell>
+                </Grid>
             </div>
          );
     }
