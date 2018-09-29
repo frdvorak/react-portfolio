6947d896fa9ebfdf4eb2615d97c89e43709c3c4f
unknown <frantisek.dvorak011@gmail.com>
2018-09-29 10:58:42 +0200

    contact page finished

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import Face from '../images/face.png';
+import {List, ListItem, ListItemContent} from 'react-mdl';
+
 
 class Contact extends Component {
     state = {  }
@@ -8,8 +11,26 @@ class Contact extends Component {
             <div className='main-page contact-body'>
                 <h1>Contact page</h1>
                 <Grid className='contact-grid'>
-                    <Cell col={6}>half page</Cell>
-                    <Cell col={6}>half page</Cell>
+                    <Cell col={6}>
+                        <h2>Frank Dvorak</h2>
+                        <img className='img' alt='img' src={Face} style={{height: '250px'}}/>
+                        <p style={{width: '75%', margin: 'auto', paddingTop: '1em', textAlign: 'justify'}}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam bibendum elit eget erat. Maecenas sollicitudin. Phasellus enim erat, vestibulum vel.</p>
+                    </Cell>
+
+                    <Cell col={6}>
+                    <h2>Contact Me</h2>
+                    <hr/>
+                    <List className='contact-list'>
+                        <ListItem>
+                            <ListItemContent icon="phone" style={{fontSize: '25px', fontFamily: 'Anton', color: 'white'}}>
+                            123 456 789</ListItemContent>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemContent icon="email" style={{fontSize: '25px', fontFamily: 'Anton', color: 'white'}}>
+                            email@gmail.com</ListItemContent>
+                        </ListItem>
+                    </List>
+                    </Cell>
                 </Grid>
             </div>
          );
